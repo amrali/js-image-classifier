8c4546ff09e24a8a64a28ac30f3fa5dea67a77c3
Amr Ali <amralicc@gmail.com>
2019-08-25 17:13:20 +0200

    Add 3 KNN classes in front of MobileNet ConvNet

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let net;
 // Get the video DOM element.
 const webcamElement = document.getElementById('webcam');
 
+// KNN classifier
+const classifier = knnClassifier.create();
+
 // Entry point.
 async function app() {
   console.log('Loading mobilenet..');
@@ -14,17 +17,38 @@ async function app() {
   // Setup video.
   await setupWebcam();
 
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  const btn_classes = ["class-a", "class-b", "class-c"];
+  btn_classes.forEach((item, index) => {
+    document.getElementById(item).addEventListener('click', () => addExample(index));
+  });
+
   // Main I/O loop to pull frames from the video feed and classify them.
   while (true) {
-    const result = await net.classify(webcamElement);
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
+      const classes = ['A', 'B', 'C'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
 
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
     await tf.nextFrame();
   }
 }
