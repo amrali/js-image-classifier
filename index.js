02052d5d4c4bf85ce12f9894e393cabc7b0c6a79
Amr Ali <amralicc@gmail.com>
2019-08-25 14:54:00 +0200

    Replace static image classification with a video stream from a webcam

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let net;
 
+// Get the video DOM element.
+const webcamElement = document.getElementById('webcam');
+
+// Entry point.
 async function app() {
   console.log('Loading mobilenet..');
 
@@ -7,10 +11,42 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+  // Setup video.
+  await setupWebcam();
+
+  // Main I/O loop to pull frames from the video feed and classify them.
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
+
+// Open video stream and return as a promise.
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
 }
 
 app();
